fa45acfd3ba4c2b1175b446791ce982e5ed7fa0f
nemosage <eclipsenemo@gmail.com>
2017-03-12 23:30:18 +0800

    登录实现

---
+++ Visual Studio Projects/ClothesProject/ClothesProject/Login.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace ClothesProject
+{
+    partial class Login
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox_username = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.button_login = new System.Windows.Forms.Button();
+            this.label_username = new System.Windows.Forms.Label();
+            this.label_password = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.label_prompt = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_username
+            // 
+            this.textBox_username.Location = new System.Drawing.Point(212, 132);
+            this.textBox_username.Name = "textBox_username";
+            this.textBox_username.Size = new System.Drawing.Size(208, 25);
+            this.textBox_username.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Location = new System.Drawing.Point(212, 186);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.PasswordChar = '*';
+            this.textBox_password.Size = new System.Drawing.Size(208, 25);
+            this.textBox_password.TabIndex = 2;
+            // 
+            // button_login
+            // 
+            this.button_login.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button_login.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_login.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button_login.Location = new System.Drawing.Point(212, 310);
+            this.button_login.Name = "button_login";
+            this.button_login.Size = new System.Drawing.Size(170, 37);
+            this.button_login.TabIndex = 3;
+            this.button_login.Text = "登录";
+            this.button_login.UseVisualStyleBackColor = false;
+            this.button_login.Click += new System.EventHandler(this.button_login_Click);
+            // 
+            // label_username
+            // 
+            this.label_username.AutoSize = true;
+            this.label_username.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_username.Location = new System.Drawing.Point(130, 140);
+            this.label_username.Name = "label_username";
+            this.label_username.Size = new System.Drawing.Size(55, 15);
+            this.label_username.TabIndex = 4;
+            this.label_username.Text = "用户：";
+            // 
+            // label_password
+            // 
+            this.label_password.AutoSize = true;
+            this.label_password.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_password.Location = new System.Drawing.Point(130, 189);
+            this.label_password.Name = "label_password";
+            this.label_password.Size = new System.Drawing.Size(55, 15);
+            this.label_password.TabIndex = 5;
+            this.label_password.Text = "密码：";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.linkLabel1.Location = new System.Drawing.Point(443, 135);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(67, 15);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "用户注册";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.linkLabel2.Location = new System.Drawing.Point(443, 189);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(67, 15);
+            this.linkLabel2.TabIndex = 7;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "忘记密码";
+            // 
+            // label_prompt
+            // 
+            this.label_prompt.AutoSize = true;
+            this.label_prompt.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_prompt.ForeColor = System.Drawing.Color.Red;
+            this.label_prompt.Location = new System.Drawing.Point(209, 240);
+            this.label_prompt.Name = "label_prompt";
+            this.label_prompt.Size = new System.Drawing.Size(0, 15);
+            this.label_prompt.TabIndex = 8;
+            // 
+            // Login
+            // 
+            this.AcceptButton = this.button_login;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 410);
+            this.Controls.Add(this.label_prompt);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label_password);
+            this.Controls.Add(this.label_username);
+            this.Controls.Add(this.button_login);
+            this.Controls.Add(this.textBox_password);
+            this.Controls.Add(this.textBox_username);
+            this.Name = "Login";
+            this.Text = "用户登录";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_username;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox_password;
+        private System.Windows.Forms.Button button_login;
+        private System.Windows.Forms.Label label_username;
+        private System.Windows.Forms.Label label_password;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Label label_prompt;
+    }
+}
+
---
+++ Visual Studio Projects/ClothesProject/ClothesProject/Login.cs
@@ -0,0 +1,66 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClothesProject
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button_login_Click(object sender, EventArgs e)
+        {
+            if ("".Equals(textBox_username))
+            {
+                label_prompt.Text = "用户名不能为空！";
+            } else
+            {
+                if ("".Equals(textBox_password))
+                {
+                    label_prompt.Text = "密码不能为空！";
+                } else
+                {
+                    connection_DB();
+                }
+            }
+        }
+        private void connection_DB()
+        {
+            string connStr = "server=localhost;user=root;database=fashion;port=3306;password=mysql_rootadmin;IgnorePrepare=false;";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                label_prompt.Text = "Connecting MySQL Database ...";
+                conn.Open();
+                string sql = "SELECT user_id,user_pass FROM user WHERE user_id=@username AND user_pass=@password";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@username", textBox_username.Text.ToString().Trim());
+                cmd.Parameters.AddWithValue("@password", textBox_password.Text.ToString().Trim());
+                cmd.Prepare();
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                if (rdr.Read() == true)  //  有数据
+                {
+                    label_prompt.Text = "欢迎登录！";
+                } else
+                {
+                    label_prompt.Text = "密码错误,请核对用户名和密码！";
+                }
+                rdr.Close();
+            } catch (MySqlException e)
+            {
+                label_prompt.Text = "Error " + e.Number + " has occurred: " + e.Message;
+            }
+            conn.Close();
+        }
+    }
+}
